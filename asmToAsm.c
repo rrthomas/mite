58c750c95ab40828acfc5703999269c8c9e8e1a8
Reuben Thomas <rrt@sc3d.org>
2001-07-26 14:43:51 +0000

    Add assembly to assembly support. This is just for checking the translator.

---
+++ asmToAsm.c
@@ -0,0 +1,12 @@
+/* Mite assembly to object translator
+   (c) Reuben Thomas 2001
+*/
+
+
+#include "translate.h"
+
+#define TRANSLATOR asmToAsm
+Translator TRANSLATOR;
+
+#include "asmWrite.c"
+#include "asmRead.c"
