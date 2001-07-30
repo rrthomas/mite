0eaed5afbdc572d8a8709207dd80928b0a69e74c
Reuben Thomas <rrt@sc3d.org>
2001-07-30 08:55:16 +0000

    New header for bits of the reader/writer interface that must be declared at the top of a translator.

---
+++ translator.h
@@ -0,0 +1,15 @@
+/* Mite translator private declarations
+   (c) Reuben Thomas 2000
+*/
+
+
+#ifndef MITE_TRANSLATOR
+#define MITE_TRANSLATOR
+
+
+#include "translate.h"
+
+static LabelValue
+labelMap(TState *t, Label *l);
+
+#endif
