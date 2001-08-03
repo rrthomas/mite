b649a95db38ef2dea5a8408c063c8c17f3c04115
Reuben Thomas <rrt@sc3d.org>
2001-08-03 14:46:02 +0000

    Use same layout rules as elsewhere

--- flen.c
+++ flen.c
@@ -10,9 +10,9 @@
 long
 flen(FILE *fp)
 {
-  long pos= ftell(fp);
+  long pos = ftell(fp);
   if (pos != -1 && fseek(fp, 0, SEEK_END) == 0) {
-    long len= ftell(fp);
+    long len = ftell(fp);
     if (len != -1 && fseek(fp, pos, SEEK_SET) == 0)
       return len;
   }
