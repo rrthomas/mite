fe5341d83b08025e32de4bda03cd6f66462b3a96
Reuben Thomas <rrt@sc3d.org>
2001-08-06 13:21:13 +0000

    Reorganise source into directories:
      src: mit source
      doc: documentation
      script: script files that are shared between src and doc (just
      util.lua for now)
      spec: Specification files (currently just insts.lua)
    
    There's a Makefile at the top level, and in src and doc, and
    mkinsts.lua has been split between src and doc.

--- Makefile
+++ Makefile
@@ -1,87 +1,21 @@
-# Makefile for Mite
+# Top-level Makefile for Mite
 # (c) Reuben Thomas 1999
 
 
-# Programs and options
+MITE = .
 
-CC = gcc
-WARN = -Wall -W -Wundef -Wpointer-arith -Wbad-function-cast -Wcast-qual \
-       -Wcast-align -Wwrite-strings -Wstrict-prototypes -Wmissing-prototypes
-COPTS = -ansi -pedantic -g # -O2 -fomit-frame-pointer 
-CFLAGS = $(COPTS) $(WARN)
-LDFLAGS = -lm
-HEVEA_FLAGS = -I texmf
+all: mit doc
 
+mit:
+	cd src; $(MAKE) all
 
-# Suffix rules
+doc:
+	cd doc; $(MAKE) all
 
-.SUFFIXES: .tex .dvi .ps .pdf .html
-.tex.dvi:
-	latex $<
-	bibtex $*
-	latex $<
-	latex $<
-.dvi.ps:
-	dvips $<
-.ps.pdf:
-	ps2pdf $<
-.tex.html:
-	hevea $(HEVEA_FLAGS) sym.hva package.hva ctable.sty $<
-
-
-# Source files
-
-TRANS_SRC = asmToObj.c objToAsm.c
-TRANS_OBJ = asmToObj.o objToAsm.o
-SRCS = mit.c translate.c except.c list.c hash.c flen.c insts.c $(TRANS_SRC)
-OBJS = mit.o translate.o except.o list.o hash.o flen.o insts.o $(TRANS_OBJ)
-
-DOCS = README TODO MANIFESTO mite.pdf iface.pdf
-TEX_TABLES = instLab.tex instComp.tex instData.tex instOpcode.tex
-
-# Top-level targets
-
-all: mit
-
-doc: $(DOCS)
-
-depend: .depend $(SRCS)
-
-
-dist: $(DOCS)
+dist: all clean
+	cd ..
 	rm -f mite.zip
-	zip -qr mite.zip Makefile README ToDo Manifesto *.pdf *.tex *.c *.h
+	zip -qr mite.zip mite
 
 clean:
 	rm -f $(OBJS) *.log *.aux *.blg *.bbl
-
-
-# File dependencies
-
-.depend: $(SRCS)
-	mkdep -p $(CFLAGS) $(SRCS)
-
-mit: $(OBJS)
-	$(CC) -o $@ $^ $(LDFLAGS)
-
-# avoid spurious warnings when compiling gperf output
-insts.o:
-	$(CC) -c $(COPTS) -o $@ $<
-
-insts.c: insts.gperf
-	gperf -L ANSI-C -N findInst -t -k 1,2,'$$' $< > $@
-
-endian.h: endian.c
-	$(CC) -o endian endian.c
-	./endian > $@
-
-insts.gperf translate.h $(TEX_TABLES): insts.lua mkinsts.lua
-	lua mkinsts.lua
-	touch mite.tex
-
-mite.tex: $(TEX_TABLES)
-
-
-# Include dependencies
-
-include .depend
--- doc/mkinsts.lua
+++ doc/mkinsts.lua
@@ -1,23 +1,21 @@
 -- Mite instruction table generator
 -- (c) Reuben Thomas 2001
 
--- Take the raw instruction table and generate:
---   1. The opcode enumeration [opEnum.h]
---   3. The array of names indexed by opcode [opToName.h]
---   2. The name -> opcode map [insts.gperf]
---   4. The documentation tables [instLab.tex, instComp.tex,
---      instData.tex, instOpcode.tex]
+-- Take the raw instruction table and generate the documentation
+-- tables instLab.tex, instComp.tex, instData.tex and instOpcode.tex
 
--- See insts.lua for the format of the instruction table
+-- See $(MITE)/spec/insts.lua for the format of the instruction table
 
 
-dofile("util.lua")
-dofile("insts.lua")
+miteDir = arg[1] -- $(MITE)
+dofile(miteDir .. "/script/util.lua") -- utility routines
+dofile(miteDir .. "/spec/insts.lua") -- instruction set spec
+
+-- Make a name->instruction index
 nameToInst = makeIndex("name", inst)
 
 -- Global formatting parameters
 width = 72 -- max width of output files
-indent = 2 -- indent of data structures
 
 -- Expand the desc fields of the instructions
 for i = 1, getn(inst) do
@@ -32,55 +30,6 @@ for i = 1, getn(inst) do
   inst[i].desc = d
 end
 
--- produce OP_NAME from name
-function opify(n)
-  return "OP_" .. strupper(n)
-end
-
-
--- opEnum.h
--- enumeration of opcodes
--- each instruction name is uppercased and prefixed with OP_
-
-writeto("opEnum.h")
-write("/* Instruction opcodes */\n\n")
-write("#ifndef MITE_OPENUM\n")
-write("#define MITE_OPENUM\n\n\n")
-write("enum {\n")
-s = opify(inst[1].name) .. " = 0x01, "
-for i = 2, getn(inst) do
-  s = s .. opify(inst[i].name) .. ", "
-end
-s = s .. opify("INSTS", getn(inst))
-write(wrap(s, width, indent, indent) .. "\n")
-write("};\n\n")
-write("#endif\n")
-
-
--- opToName.h
--- char * array of instruction names in opcode order
-writeto("opToName.h")
-s = "\"\", " -- blank entry for 0; opcodes start at 0x01
-for i = 1, getn(inst) do
-  s = s .. "\"" .. inst[i].name .. "\", "
-end
-write(wrap(s, width, indent, indent) .. "\n")
-
-
--- insts.gperf
--- list of opcode, name pairs in alphabetical order
-writeto("insts.gperf")
-write("%{\n")
-write("#include \"insts.h\"\n")
-write("%}\n")
-write("struct Inst { const char *name; unsigned int opcode; };\n")
-write("%%\n")
-for i = 1, getn(inst) do
-  write(inst[i].name .. ", " .. opify(inst[i].name) .. "\n")
-end
-
-
--- LaTeX fragments
 -- write LaTeX table lines for the given instructions
 function writeTable(...)
   for i = 1, arg.n do
