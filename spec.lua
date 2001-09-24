60f28190156e532dd58e10c90115a467b0f4cf1d
Reuben Thomas <rrt@sc3d.org>
2001-09-24 09:51:48 +0000

    Removed L, b, s, d types; b, s and d become l, and rather than l, have n (for "name"). This simplifies the type scheme, and unifies the assembly syntax (where lab takes a type and a name) with the abstract syntax. The condition that most instructions require a particular type of label is moved to the semantics, where instead of writing "ln" for a label parameter, we write "tln", where t is the type.

--- spec.lua
+++ spec.lua
@@ -8,17 +8,15 @@
 --   * Each type has two fields:
 Type = constructor{
   "name", -- as in the assembly language
-  "desc", -- informal description
+  "desc", -- description
 }
 
-type = {
-  Type("r",    "register"),
-  Type("i",    "immediate constant"),
-  Type("l",    "label (any type)"),
-  Type("L",    "label definition"),
-  Type("b",    "branch label"),
-  Type("s",    "subroutine label"),
-  Type("d",    "data label"),
+opType = {
+  Type("r", "register"),
+  Type("i", "immediate constant"),
+  Type("t", "label type"),
+  Type("l", "label"),
+  Type("n", "name"),
 }
 
 
@@ -27,17 +25,17 @@ type = {
 --   * Opcode is given by list position (e.g. lab = 0x1)
 --   * Each instruction has three fields:
 Inst = constructor{
-  "name", -- as in the assembly language
-  "ops",  -- list of operand types (see types.lua)
-  "desc", -- informal semantics
-          -- (%n --> {ops[n]}_{n}; %% --> %; <- --> \gets)
+  "name",   -- as in the assembly language
+  "ops",    -- list of operand types (see types.lua)
+  "effect", -- semantics
+            -- (%n --> {ops[n]}_{n}; %% --> %; <- --> \gets)
 }
 
 inst = {
-  Inst("lab",    {"L"},            "define a label"),
+  Inst("lab",    {"t", "n"},       "define a type $%1$ label named $%2$"),
   Inst("mov",    {"r", "r"},       "$%1<-%2$"),
   Inst("movi",   {"r", "i"},       "$%1<-%2$"),
-  Inst("ldl",    {"r", "d"},       "$%1<-%2$"),
+  Inst("ldl",    {"r", "l"},       "$%1<-\syn{d}%2$"),
   Inst("ld",     {"r", "r"},       "$%1<-M(%2)$"),
   Inst("st",     {"r", "r"},       "$M(%2)<-%1$"),
   Inst("gets",   {"r"},            "$%1<-S$"),
@@ -58,15 +56,15 @@ inst = {
   Inst("teq",    {"r", "r", "r"},  "$%1<-\\{%2=%3\\}$"),
   Inst("tlt",    {"r", "r", "r"},  "$%1<-\\{%2<%3\\}$"),
   Inst("tltu",   {"r", "r", "r"},  "$%1<-\\{%2<%3$ (unsigned)$\\}$"),
-  Inst("b",      {"b"},            "$P<-%1$"),
+  Inst("b",      {"l"},            "$P<-\syn{b}%1$"),
   Inst("br",     {"r"},            "$P<-%1$"),
-  Inst("bf",     {"r", "b"},       "if $%1=0$, $P<-%2$"),
-  Inst("bt",     {"r", "b"},       "if $%1\\neq0$, $P<-%2$"),
-  Inst("call",   {"s"},            "$S<-S+sw$; $M(S)<-P$; $P<-%1$"),
+  Inst("bf",     {"r", "l"},       "if $%1=0$, $P<-\syn{b}%2$"),
+  Inst("bt",     {"r", "l"},       "if $%1\\neq0$, $P<-\syn{b}%2$"),
+  Inst("call",   {"l"},            "$S<-S+sw$; $M(S)<-P$; $P<-\syn{s}%1$"),
   Inst("callr",  {"r"},            "$S<-S+sw$; $M(S)<-P$; $P<-%1$"),
   Inst("ret",    {},               "$P<-M(S)$; $S<-S-sw$"),
   Inst("calln",  {"r"},            "call native code at $%1$"),
   Inst("lit",    {"i"},            "a literal word"),
-  Inst("litl",   {"l"},            "a literal label"),
+  Inst("litl",   {"t", "l"},       "a literal label"),
   Inst("space",  {"i"},            "$%1$ zero words ($%1>0$)"),
 }
