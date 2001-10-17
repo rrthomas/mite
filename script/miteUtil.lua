c7463b1f49ad0d787902253c6e270a0679e67321
Reuben Thomas <rrt@sc3d.org>
2001-10-17 11:37:13 +0000

    Add global formatting parameters (no particular reason not to use the same settings for source and docs). Also add the writeWrapped function here, which uses them.

--- script/miteUtil.lua
+++ script/miteUtil.lua
@@ -2,6 +2,16 @@
 -- (c) Reuben Thomas 2001
 
 
+-- Global formatting parameters
+width = 72 -- max width of output files
+indent = 2 -- default indent
+
+-- write a string wrapped with the above parameters, with a
+-- terminating newline
+function writeWrapped(s)
+  writeLine(wrap(s, width, indent))
+end
+
 -- produce OP_NAME from name
 function opify(n)
   return "OP_" .. strupper(n)
