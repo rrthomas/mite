89f1eaeda55c46a9181694afbfbf0def9b45d130
Reuben Thomas <rrt@sc3d.org>
2001-10-22 13:26:14 +0000

    New general utility header; imports all the rest of the utility headers.

---
+++ src/util.h
@@ -0,0 +1,23 @@
+/* General utilities
+   (c) Reuben Thomas 2000
+*/
+
+
+#ifndef MITE_UTIL
+#define MITE_UTIL
+
+
+#include <stdint.h>
+
+#include "endian.h"
+#include "except.h"
+#include "buffer.h"
+#include "list.h"
+#include "hash.h"
+#include "flen.h"
+
+
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#define max(a, b) ((a) > (b) ? (a) : (b))
+
+#endif
