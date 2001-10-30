433d169e3b7181b63fb4daa4f263bb6c55a45fa0
Reuben Thomas <rrt@sc3d.org>
2001-10-30 10:07:10 +0000

    Generate exception codes (excEnum.h) and messages (excMsg.h) from a spec. This ties in with now throwing numeric codes instead of string pointers, ensures that standard messages are used and that strings aren't duplicated.

---
+++ src/except.lua
@@ -0,0 +1,36 @@
+Exc = constructor{
+  "name",
+  "message", -- printed when throw reaches the top level
+}
+
+exception = {
+  -- fundamental errors
+  Exc("Malloc", "could not allocate memory"),
+  Exc("Realloc", "could not reallocate memory"),
+  -- Mite errors
+  Exc("Ret", "ret on empty stack"),
+  Exc("BadInst", "invalid instruction"),
+  Exc("BadReg", "invalid register"),
+  Exc("BadLab", "invalid label"),
+  Exc("BadLabTy", "invalid label type"),
+  Exc("BadP", "invalid P"),
+  Exc("BadS", "invalid S"),
+  Exc("BadImm", "invalid immediate"),
+  Exc("BadImmVal", "immediate value out of range"),
+  Exc("BadImmRot", "immediate rotation out of range"),
+  Exc("BadImmMod", "invalid immediate modifiers"),
+  Exc("BadAddr", "invalid address"),
+  Exc("DivZero", "division by zero"),
+  Exc("BadShift", "invalid shift"),
+  -- I/O errors
+  Exc("Fopen", "could not open `%s'"),
+  Exc("Fread", "error reading `%s'"),
+  Exc("Fwrite", "error writing `%s'"),
+  Exc("Flen", "error finding length of `%s'"),
+  Exc("EmptyFile", "empty file `%s'"),
+  -- Translator errors
+  Exc("MissingTok", "missing token"),
+  Exc("BadChar", "invalid character"),
+  Exc("WrongLab", "inconsistent label"),
+  Exc("DupLab", "duplicate definition for `%s'")
+}
---
+++ src/mkExcept.lua
@@ -0,0 +1,27 @@
+-- Generate exception headers from the exception table
+-- (c) Reuben Thomas 2001
+
+-- Take the exception table and generate:
+--   * The exception enumeration [excEnum.h]
+--   * List of exception messages [excMsg.h]
+
+dofile(arg[1]) -- load the exceptions table
+
+
+-- excEnum.h
+-- enumeration of opcodes
+function prefix(s)
+  return "Exc" .. s
+end
+writeto("excEnum.h")
+excEnum = map(prefix, project(exception, "name"))
+excEnum[1] = excEnum[1] .. " = 0x01"
+writeWrapped(join(", ", excEnum))
+
+-- excMsg.h
+-- list of messages
+function quote(s)
+  return "\"" .. s .. "\""
+end
+writeto("excMsg.h")
+writeLine(join(",\n", map(quote, project(exception, "message"))))
