d9a31af89f03bff774f57a8bc196b530d90f6246
Reuben Thomas <rrt@sc3d.org>
2002-02-17 15:59:21 +0000

    These files are now generated by mkTrans.lua.

--- src/translators.c
+++
@@ -1,9 +0,0 @@
-/* Array of translators */
-
-#include "translators.h"
-
-TState *(*translator[Interp][Interp])(Byte *, Byte *) =
-{{NULL, &objToAsm, &objToInterp},
- {&asmToObj, NULL, NULL},
- {NULL, NULL, NULL}
-};
