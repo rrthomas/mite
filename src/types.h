1bc6fffe7826daba566ace6d000077b952758896
Reuben Thomas <rrt@sc3d.org>
2002-02-17 15:51:20 +0000

    Types separated out from translate.h so they can be used in Mite support libraries.

---
+++ src/types.h
@@ -0,0 +1,21 @@
+/* Mite translator basic types
+   (c) Reuben Thomas 2000
+*/
+
+
+#ifndef MITE_TYPES
+#define MITE_TYPES
+
+
+#include <stdint.h>
+
+
+typedef int Bool;
+typedef uint8_t Byte;
+typedef int8_t SByte;
+typedef uint32_t InstWord;
+typedef int32_t SInstWord;
+typedef uintptr_t Word;
+typedef intptr_t SWord;
+
+#endif
