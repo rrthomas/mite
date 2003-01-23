ce74df19910020218d4628cf3a43c2d93630ef2b
Reuben Thomas <rrt@sc3d.org>
2003-01-23 13:04:40 +0000

    Add obj -> run translator (the interpreter).

--- src/transList.lua
+++ src/transList.lua
@@ -1,9 +1,10 @@
--- List of translators (reader-writer pairs)
+-- List of translators
 -- (c) Reuben Thomas 2002
 
+-- Each list has the form {reader, writer, {list-of-instrumenters}}
 return {
   {"asm", "obj"},
   {"obj", "asm"},
   {"obj", "interp"},
-  {"obj", "run"},
+  {"obj", "run", {"interpDebug"}},
 }
