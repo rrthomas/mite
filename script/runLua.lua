7e5452d0ac376abbc6eea34a7e68f0ce4bbdc58c
Reuben Thomas <rrt@sc3d.org>
2003-01-23 12:43:53 +0000

    Use new require syntax.

--- script/runLua.lua
+++ script/runLua.lua
@@ -3,7 +3,7 @@
 
 miteDir, script = arg[1], arg[2] -- $(MITE), name of script
 prog = { name = gsub (arg[2], "%..*$", "") }
-require "std.lua" -- standard library
+require "std" -- standard library
 shift (2) -- remove the arguments
 dofile (miteDir .. "/script/miteUtil.lua") -- Mite-specific utilities
 dofile (miteDir .. "/spec.lua") -- Mite's specification
