96df0f1caceed21de3114a96d0d1ef655151760d
Reuben Thomas <rrt@sc3d.org>
2003-02-07 00:55:05 +0000

    Add C call instructions, rename types.tex to opTypes.tex.

--- doc/procSpec.lua
+++ doc/procSpec.lua
@@ -36,14 +36,18 @@ writeTable ("lab")
 
 -- instComp.tex: the computational instructions
 writeto ("instComp.tex")
-writeTable ("mov", "movi", "ldl", "ld", "st", "gets", "sets", "pop",
-            "push", "add", "sub", "mul", "div", "rem", "and", "or",
-            "xor", "sl", "srl", "sra", "teq", "tlt", "tltu", "b",
-            "br", "bf", "bt", "call", "callr", "ret", "calln")
+writeTable ("mov", "movi", "ldl", "ld", "st", "ldo", "sto", "add",
+            "sub", "mul", "div", "rem", "and", "or", "xor", "sl",
+            "srl", "sra", "teq", "tlt", "tltu", "b", "br", "bf", "bt",
+            "call", "callr", "ret", "salloc")
 
 -- instData.tex: the data instructions
 writeto ("instData.tex")
 writeTable ("lit", "litl", "space")
+
+-- instCcall.tex: the C call instructions
+writeto ("instCcall.tex")
+writeTable ("func", "funcv", "arg", "callf", "callfr", "retf", "retfv")
 
 -- instOp.tex: the opcode table (3 columns)
 function opEntry (n)
@@ -58,8 +62,8 @@ for i = 1, rows do
              opEntry (i + rows * 2) .. " \\\\")
 end
 
--- types.tex: the operand types
-writeto ("types.tex")
+-- opTypes.tex: the operand types
+writeto ("opTypes.tex")
 for i = 1, getn (opType) do
   writeLine (opType[i].name .. " & " .. opType[i].desc .. " \\\\")
 end
