b923b04284a11a0df97644a7ea491445ff3d4aec
Reuben Thomas <rrt@sc3d.org>
2019-05-08 06:00:35 +0100

    src/objRead.lua: implement OpType constructor

--- src/objRead.lua
+++ src/objRead.lua
@@ -2,7 +2,31 @@
 -- (c) Reuben Thomas 2000
 
 
-return {
+OpType =
+  function (arg)
+    local decl, code = arg[1], arg[2]
+    -- decl: declarations for the operand reading code
+    -- code: code to read an operand of the given type
+    -- each is either a string, or a function (inst, op) ->
+    -- string
+    -- in the output, %n -> operand no.
+    function funcify (f)
+      if type (f) ~= "function" then
+        local _f = f
+        f = function (inst, op)
+          return _f
+        end
+      end
+      return function (inst, opNo)
+        local s = f (inst, opNo)
+        return string.gsub (s, "%%n", tostring (opNo))
+      end
+    end
+    local t = {decl = funcify (decl), code = funcify (code)}
+    return t
+  end
+
+local r = {
   reads = "Mite object code",
   input = "typedef interpW_Output objR_Input;",
   prelude =
@@ -116,42 +140,7 @@ objR_readerNew (objR_Input *inp)
   R->end = inp->cImg + inp->cSize;
   return R;
 }]],
-  opType = {
-    r = OpType {[[#undef r%n
-        #define r%n objR_op%n]],
-        [[if (r%n == 0 || r%n > REGISTER_MAX)
-          diePos (R, ExcBadRegister);]]},
-    i = OpType {[[#undef i%n_f
-        #define i%n_f objR_op%n
-        int i%n_sgn, i%n_r;
-        Word i%n_v;]],
-               function (inst, op)
-                 return
-        [[if (i%n_f & ~(FLAG_R | FLAG_S | FLAG_W | FLAG_E))
-          diePos (R, ExcBadImmMod);
-        i%n_r = (objR_op%n & FLAG_R) ?
-          (R->ptr -= 2 - %n, objR_op]] .. tostring (op + 1) ..
-          [[) : ((R->ptr -= 3 - %n), 0);
-        i%n_v = objR_getNum (R, &i%n_sgn);]]
-               end},
-    t = OpType {[[#undef t%n
-        #define t%n objR_op%n]],
-        [[if (objR_op%n == 0 || objR_op%n > LABEL_TYPES)
-          diePos (R, ExcBadLabelType);]]},
-    l = OpType {[[int l%n_sgn;
-        LabelValue l%n;]],
-               function (inst, op)
-                 return [[R->ptr -= 4 - %n;
-        l%n.n = objR_getNum (R, &l%n_sgn);
-        if (l%n_sgn)
-          diePos (R, ExcBadLabel);]]
-               end},
-    n = OpType {"LabelValue l%n;",
-               function (inst, op)
-                 return "l%n.n = ++T->labels[t" .. tostring (op - 1) ..
-                   "];"
-               end},
-  },
+  inst = {},
   trans = Translator {
     [[InstWord objR_w;
   Byte objR_op1, objR_op2, objR_op3;]],       -- decls
@@ -165,3 +154,87 @@ objR_readerNew (objR_Input *inp)
     "",                                       -- finish
   },
 }
+
+rOpType = {
+  r = OpType {[[#undef r%n
+      #define r%n objR_op%n]],
+      [[if (r%n == 0 || r%n > INT_REGISTER_MAX) /* FIXME: have a value for R */
+        diePos (R, ExcBadRegister);]]},
+  s = OpType {[[#undef s%n
+      #define s%n objR_op%n]],
+      [[if (s%n == 0 || s%n & (s%n - 1))    /* Sizes have exactly one bit set */
+        die(ExcBadSize);]]},
+  i = OpType {[[#undef i%n_f
+      #define i%n_f objR_op%n
+      int i%n_sgn, i%n_r;
+      Word i%n_v;]],
+             function (inst, op)
+               return
+      [[if (i%n_f & ~(FLAG_R | FLAG_S | FLAG_W | FLAG_E))
+        diePos (R, ExcBadImmMod);
+      i%n_r = (objR_op%n & FLAG_R) ?
+        (R->ptr -= 2 - %n, objR_op]] .. tostring (op + 1) ..
+        [[) : ((R->ptr -= 3 - %n), 0);
+      i%n_v = objR_getNum (R, &i%n_sgn);]]
+             end},
+  n = OpType {"LabelValue l%n;",
+             function (inst, op)
+               return "l%n.n = ++T->labels[t" .. tostring (op - 1) ..
+                 "];"
+             end},
+  t = OpType {[[#undef t%n
+      #define t%n objR_op%n]],
+      [[if (objR_op%n == 0 || objR_op%n > LABEL_TYPES)
+        diePos (R, ExcBadLabelType);]]},
+  l = OpType {[[int l%n_sgn;
+      LabelValue l%n;]],
+             function (inst, op)
+               return [[R->ptr -= 4 - %n;
+      l%n.n = objR_getNum (R, &l%n_sgn);
+      if (l%n_sgn)
+        diePos (R, ExcBadLabel);]]
+             end},
+  x = OpType {},
+  a = OpType {},
+}
+r.opType["R"] = r.opType["r"]
+
+-- FIXME: The rest should be in mkTrans.lua or util.lua
+
+-- Check the reader implements the correct opTypes
+checkReaderOpTypes (opType, r, rOpType)
+
+-- Compute the instruction definitions
+for i = 1, #inst do
+  local inst = inst[i]
+  local decl, code = "", ""
+
+  -- Add the declarations
+  for j = 1, #inst.ops do
+    local opType, opRepeat = getOpInfo (inst.ops[j])
+    local opDecl = rOpType[opType].decl (inst, j)
+    if opDecl ~= "" then
+      decl = decl .. opDecl .. "\n"
+    end
+  end
+
+  -- Add the code
+  for j = 1, #inst.ops do
+    local opType, opRepeat = getOpInfo (inst.ops[j])
+    local opCode = rOpType[opType].code (inst, j)
+    if opCode ~= "" then
+      if opRepeat then
+        code = code .. "for (size_t i = 1; i <= n" .. tostring (j - 1)
+          .. "; i++) {\n  "
+      end
+      code = code .. opCode .. "\n"
+      if opRepeat then
+        code = code .. "}\n"
+      end
+    end
+  end
+
+  r.inst[i] = Inst {inst.name, decl, code}
+end
+
+return r
